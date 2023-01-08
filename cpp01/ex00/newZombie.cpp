7dd86117d9e5f5f68bc9a5847fc1311e656dd224
Mitsu325 <patricia.mashiba@gmail.com>
2023-01-08 10:43:13 -0300

    feat: newZombie, save Zombie on heap

---
+++ cpp01/ex00/newZombie.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 10:06:17 by pmitsuko          #+#    #+#             */
+/*   Updated: 2023/01/08 10:24:45 by pmitsuko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie* newZombie(std::string name)
+{
+	Zombie	*zombie = new Zombie(name);
+
+	zombie->announce();
+	return zombie;
+}
