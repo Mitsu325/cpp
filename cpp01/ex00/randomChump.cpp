8deb81423f27c58db56f1c650e154f7a5f05c464
Mitsu325 <patricia.mashiba@gmail.com>
2023-01-08 10:43:28 -0300

    feat: randomChump, save Zombie on stack

---
+++ cpp01/ex00/randomChump.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 10:06:30 by pmitsuko          #+#    #+#             */
+/*   Updated: 2023/01/08 10:33:15 by pmitsuko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void	randomChump(std::string name)
+{
+	Zombie zombie(name);
+
+	zombie.announce();
+	return ;
+}
