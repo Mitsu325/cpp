b71647a5b73bda996bb1915465dcb00ca2e96122
Mitsu325 <patricia.mashiba@gmail.com>
2023-01-10 22:54:58 -0300

    feat: FileReplace class

---
+++ cpp01/ex04/FileReplace.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FileReplace.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 20:59:18 by pmitsuko          #+#    #+#             */
+/*   Updated: 2023/01/08 21:00:58 by pmitsuko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FileReplace.hpp"
+
+FileReplace::FileReplace(void)
+{
+	std::cout << "FileReplace destructor" << std::endl;
+}
+
+FileReplace::~FileReplace(void)
+{
+	std::cout << "FileReplace destructor" << std::endl;
+}
---
+++ cpp01/ex04/FileReplace.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FileReplace.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 20:59:15 by pmitsuko          #+#    #+#             */
+/*   Updated: 2023/01/10 05:52:17 by pmitsuko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef FILE_REPLACE_H
+
+# include <string>
+# include <iostream>
+
+class FileReplace
+{
+	private:
+
+
+	public:
+		FileReplace(void);
+		~FileReplace(void);
+};
+
+#endif
