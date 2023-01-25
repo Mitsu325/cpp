b239bf0ded00d016bbf9dea1bad56d0c029490f4
Mitsu325 <patricia.mashiba@gmail.com>
2023-01-24 21:19:52 -0300

    feat: create ScavTrap class

---
+++ cpp03/ex02/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/22 20:38:19 by pmitsuko          #+#    #+#             */
+/*   Updated: 2023/01/24 21:15:05 by pmitsuko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+int main(void)
+{
+	ClapTrap	marvin("Marvin");
+	ScavTrap	selene("Selene");
+	FragTrap	bill("Bill");
+	FragTrap	bee("Bee");
+
+	std::cout << marvin << std::endl;
+	std::cout << selene << std::endl;
+	std::cout << bill << std::endl;
+	std::cout << bee << std::endl;
+	bee.attack(bill.getName());
+	bill.takeDamage(bee.getAttackDamage());
+	bill.beRepaired(12);
+	selene.guardGate();
+	bill.highFivesGuys();
+	std::cout << marvin << std::endl;
+	std::cout << selene << std::endl;
+	std::cout << bill << std::endl;
+	std::cout << bee << std::endl;
+	return (0);
+}
