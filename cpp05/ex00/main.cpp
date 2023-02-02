f4ef90f732ff691c50d4d75d04221fbb96733145
Mitsu325 <patricia.mashiba@gmail.com>
2023-02-02 20:50:18 -0300

    feat: test Bureaucrat

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,32 +6,85 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 20:38:19 by pmitsuko          #+#    #+#             */
-/*   Updated: 2023/01/23 21:11:34 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2023/02/02 20:48:52 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#include "Bureaucrat.hpp"
 
 int main(void)
 {
-	ClapTrap marvin("Marvin");
-	ClapTrap norm("Norm");
-
-	marvin.setAttackDamage(7);
-
-	std::cout << marvin << std::endl;
-	std::cout << norm << std::endl;
-
-	marvin.attack(norm.getName());
-	norm.takeDamage(marvin.getAttackDamage());
-
-	std::cout << marvin << std::endl;
-	std::cout << norm << std::endl;
-
-	norm.beRepaired(5);
-
-	std::cout << marvin << std::endl;
-	std::cout << norm << std::endl;
-
+	{
+		std::cout << "========== CONSTRUCTOR ===========" << std::endl;
+		Bureaucrat bob;
+		std::cout << bob << std::endl;
+		Bureaucrat john("John");
+		std::cout << john << std::endl;
+		Bureaucrat ted("Ted", 20);
+		std::cout << ted << std::endl;
+		Bureaucrat ted_copy(ted);
+		std::cout << ted_copy << std::endl;
+		bob = ted_copy;
+		std::cout << bob << std::endl;
+	}
+	{
+		std::cout << "============= GRADE ==============" << std::endl;
+		int arr[] = {-20, -1, 0, 1, 3, 5, 80, 142, 149, 150, 151, 162};
+		int arr_len = sizeof(arr) / sizeof (arr[0]);
+		for (int i = 0; i < arr_len; ++i)
+		{
+			std::cout << "Test number = " << arr[i] << std::endl;
+			try
+			{
+				Bureaucrat jimmy("Jimmy", arr[i]);
+				std::cout << jimmy << std::endl;
+			}
+			catch (std::exception& exc)
+			{
+				std::cerr << exc.what() << std::endl;
+			}
+		}
+	}
+	{
+		std::cout << "=========== INCREMENT ============" << std::endl;
+		try
+		{
+			Bureaucrat judy("Judy", 2);
+			std::cout << judy << std::endl;
+			judy.decrementGrade();
+			std::cout << judy << std::endl;
+			judy.incrementGrade();
+			std::cout << judy << std::endl;
+			judy.incrementGrade();
+			std::cout << judy << std::endl;
+			judy.incrementGrade();
+			std::cout << judy << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+	{
+		std::cout << "=========== DECREMENT ============" << std::endl;
+		try
+		{
+			Bureaucrat judy("Judy", 149);
+			std::cout << judy << std::endl;
+			judy.incrementGrade();
+			std::cout << judy << std::endl;
+			judy.decrementGrade();
+			std::cout << judy << std::endl;
+			judy.decrementGrade();
+			std::cout << judy << std::endl;
+			judy.decrementGrade();
+			std::cout << judy << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+	std::cout << "==================================" << std::endl;
 	return (0);
 }
