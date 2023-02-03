b1536a4251c6984118e00622511a44793e114f53
Mitsu325 <patricia.mashiba@gmail.com>
2023-02-03 20:59:46 -0300

    feat: change message on overload insertion

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -6,21 +6,27 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 19:48:34 by pmitsuko          #+#    #+#             */
-/*   Updated: 2023/02/03 20:17:31 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2023/02/03 20:38:40 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
-Form::Form(void): _name(""), _grade_sign(150), _grade_execute(150),
+Form::Form(void):
+	_name(""),
+	_grade_sign(150),
+	_grade_execute(150),
 	_is_signed(false)
 {
 	std::cout << "Form default constructor called" << std::endl;
 	return ;
 }
 
-Form::Form(std::string name, int grade_sign, int grade_execute): _name(name),
-	_grade_sign(grade_sign), _grade_execute(grade_execute), _is_signed(false)
+Form::Form(std::string name, int grade_sign, int grade_execute):
+	_name(name),
+	_grade_sign(grade_sign),
+	_grade_execute(grade_execute),
+	_is_signed(false)
 {
 	std::cout << "Form constructor called" << std::endl;
 	if (grade_sign < 1 || grade_execute < 1)
@@ -35,12 +41,11 @@ Form::Form(std::string name, int grade_sign, int grade_execute): _name(name),
 
 Form::Form(Form const &obj):
 	_name(obj.getName()),
-	_is_signed(obj.getSign()),
 	_grade_sign(obj.getGradeSign()),
-	_grade_execute(obj.getGradeExecute())
+	_grade_execute(obj.getGradeExecute()),
+	_is_signed(obj.getSign())
 {
 	std::cout << "Form copy constructor called" << std::endl;
-	*this = obj;
 	return ;
 }
 
@@ -112,9 +117,9 @@ const char* Form::AlreadySignedException::what() const throw()
 
 std::ostream&	operator<<(std::ostream &stream, Form const &obj)
 {
-	stream << obj.getName() << ", Form grade required to sign it is "
-		<< obj.getGradeSign() << " and grade required to execute it is "
-		<< obj.getGradeExecute() << "."
-		<< std::endl;
+	stream << "Form: " << obj.getName() << std::endl;
+	stream << "Sign: " << obj.getSign() << std::endl;
+	stream << "Grade to sign: " << obj.getGradeSign() << std::endl;
+	stream << "Grade to execute: " << obj.getGradeExecute() << std::endl;
 	return (stream);
 }
