bf45a9a1e3a49430a1fa5985b09d3730b347c222
Mitsu325 <patricia.mashiba@gmail.com>
2023-02-04 16:19:17 -0300

    feat: create test function

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 20:38:19 by pmitsuko          #+#    #+#             */
-/*   Updated: 2023/02/04 15:08:51 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2023/02/04 16:17:43 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,95 +14,71 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
-
-void	shrubbery_creation_form(void)
-{
-	std::cout << "========= SHRUBBERY EXEC FORM ==========" << std::endl;
-	try
-	{
-		ShrubberyCreationForm	scf("Shrubbery");
-		Bureaucrat				bob("Bob", 140);
-		scf.beSigned(bob);
-		std::cout << "------" << std::endl;
-		bob.executeForm(scf);
-		std::cout << "------" << std::endl;
-		Bureaucrat				ted("Ted", 135);
-		std::cout << "------" << std::endl;
-		ted.executeForm(scf);
-		std::cout << "------" << std::endl;
-		Bureaucrat				judy("Judy", 148);
-		std::cout << "------" << std::endl;
-		judy.executeForm(scf);
-		std::cout << "------" << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-	std::cout << "========================================" << std::endl;
-}
-
-void	robotomy_request_form(void)
-{
-	std::cout << "========= ROBOTOMY EXEC FORM ===========" << std::endl;
-	try
-	{
-		RobotomyRequestForm	rrf("Robot");
-		Bureaucrat				bob("Bob", 72);
-		rrf.beSigned(bob);
-		std::cout << "------" << std::endl;
-		bob.executeForm(rrf);
-		std::cout << "------" << std::endl;
-		Bureaucrat				ted("Ted", 40);
-		std::cout << "------" << std::endl;
-		ted.executeForm(rrf);
-		std::cout << "------" << std::endl;
-		Bureaucrat				judy("Judy", 73);
-		std::cout << "------" << std::endl;
-		judy.executeForm(rrf);
-		std::cout << "------" << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-	std::cout << "=========================================" << std::endl;
-}
-
-void	presidential_pardon_form(void)
-{
-	std::cout << "========== PARDON EXEC FORM =============" << std::endl;
-	try
-	{
-		PresidentialPardonForm	ppf("Robot");
-		Bureaucrat				bob("Bob", 20);
-		ppf.beSigned(bob);
-		std::cout << "------" << std::endl;
-		bob.executeForm(ppf);
-		std::cout << "------" << std::endl;
-		Bureaucrat				ted("Ted", 2);
-		std::cout << "------" << std::endl;
-		ted.executeForm(ppf);
-		std::cout << "------" << std::endl;
-		Bureaucrat				judy("Judy", 26);
-		std::cout << "------" << std::endl;
-		judy.executeForm(ppf);
-		std::cout << "------" << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-	std::cout << "=========================================" << std::endl;
-}
+#include "AForm.hpp"
+#include "Intern.hpp"
 
 int	main(void)
 {
-	shrubbery_creation_form();
-	robotomy_request_form();
-	presidential_pardon_form();
+	Intern	intern;
+	AForm	*scf;
+	AForm	*rrf;
+	AForm	*ppf;
+	AForm	*other;
+
+	std::cout << "========== MAKE FORM =============" << std::endl;
+	try
+	{
+		scf = intern.makeForm("shrubbery creation", "Shrubbery");
+		rrf = intern.makeForm("robotomy request", "Robot");
+		ppf = intern.makeForm("presidential pardon", "Presidential");
+		other = intern.makeForm("other form", "Other");
+		(void)other;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		delete scf;
+		delete rrf;
+		delete ppf;
+	}
+	std::cout << std::endl;
+	std::cout << "========== SIGN FORM =============" << std::endl;
+	try
+	{
+		Bureaucrat	bob("Bob", 5);
+		scf = intern.makeForm("shrubbery creation", "Shrubbery");
+		rrf = intern.makeForm("robotomy request", "Robot");
+		ppf = intern.makeForm("presidential pardon", "Presidential");
+		std::cout << std::endl;
+		std::cout << bob << std::endl;
+		std::cout << *scf << std::endl;
+		std::cout << *rrf << std::endl;
+		std::cout << *ppf << std::endl;
+		std::cout << std::endl;
+
+		scf->beSigned(bob);
+		std::cout << *scf << std::endl;
+		scf->execute(bob);
+		std::cout << std::endl;
+
+		rrf->beSigned(bob);
+		std::cout << *rrf << std::endl;
+		rrf->execute(bob);
+		std::cout << std::endl;
+
+		ppf->beSigned(bob);
+		std::cout << *ppf << std::endl;
+		ppf->execute(bob);
+		std::cout << std::endl;
+
+		delete scf;
+		delete rrf;
+		delete ppf;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl;
 	return (0);
 }
