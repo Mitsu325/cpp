923dd035b5942bdb6e7c60c0395e30e423160374
Mitsu325 <patricia.mashiba@gmail.com>
2023-02-03 21:00:12 -0300

    feat: create form test function

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -6,13 +6,13 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 20:38:19 by pmitsuko          #+#    #+#             */
-/*   Updated: 2023/02/02 20:48:52 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2023/02/03 20:57:28 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-int main(void)
+void	bureaucrat_test(void)
 {
 	{
 		std::cout << "========== CONSTRUCTOR ===========" << std::endl;
@@ -86,5 +86,103 @@ int main(void)
 		}
 	}
 	std::cout << "==================================" << std::endl;
+}
+
+void	form_test(void)
+{
+	{
+		std::cout << "========== CONSTRUCTOR ===========" << std::endl;
+		Form form1;
+		std::cout << form1 << std::endl;
+		Form form2("Form 2", 5, 10);
+		std::cout << form2 << std::endl;
+		Form form3("Form 3", 50, 70);
+		std::cout << form3 << std::endl;
+		Bureaucrat teddy("Teddy", 10);
+		std::cout << teddy << std::endl;
+		Form form3_copy(form3);
+		std::cout << form3_copy << std::endl;
+		form3_copy.beSigned(teddy);
+		std::cout << "--- Copy form 3 ---" << std::endl;
+		std::cout << form3_copy << std::endl;
+		std::cout << "--- Base form 3 ---" << std::endl;
+		std::cout << form3 << std::endl;
+		form1 = form3_copy;
+		std::cout << form1 << std::endl;
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "============= GRADE ==============" << std::endl;
+		int arr[] = {-20, -1, 0, 1, 3, 5, 80, 142, 149, 150, 151, 162};
+		int arr_len = sizeof(arr) / sizeof (arr[0]);
+		for (int i = 0; i < arr_len; ++i)
+		{
+			std::cout << "Test number = " << arr[i] << std::endl;
+			try
+			{
+				Form form1("Form 1", arr[i], 10);
+				std::cout << form1 << std::endl;
+			}
+			catch (std::exception& exc)
+			{
+				std::cerr << exc.what() << std::endl;
+			}
+			std::cout << "-------" << std::endl;
+			try
+			{
+				Form form2("Form 2", 20, arr[i]);
+				std::cout << form2 << std::endl;
+			}
+			catch (std::exception& exc)
+			{
+				std::cerr << exc.what() << std::endl;
+			}
+			std::cout << "-------" << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "========= SIGN FORM ==========" << std::endl;
+		try
+		{
+			Bureaucrat july("July", 10);
+			std::cout << july << std::endl;
+			Form form1("Form 1", 10, 20);
+			std::cout << form1 << std::endl;
+			july.signForm(form1);
+			std::cout << form1 << std::endl;
+			july.signForm(form1);
+			std::cout << "-------" << std::endl;
+
+			Form form2("Form 2", 15, 20);
+			std::cout << form2 << std::endl;
+			july.signForm(form2);
+			std::cout << form2 << std::endl;
+			july.signForm(form2);
+			std::cout << "-------" << std::endl;
+
+			Form form3("Form 3", 5, 20);
+			std::cout << form3 << std::endl;
+			july.signForm(form3);
+			std::cout << form3 << std::endl;
+			july.signForm(form3);
+			std::cout << "-------" << std::endl;
+
+			Form form4("Form 4", 4, 20);
+			std::cout << form4 << std::endl;
+			form4.beSigned(july);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+	std::cout << "==================================" << std::endl;
+}
+
+int	main(void)
+{
+	// bureaucrat_test();
+	form_test();
 	return (0);
 }
