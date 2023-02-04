2a139544869506ef6dd6a315fa123b299595043d
Mitsu325 <patricia.mashiba@gmail.com>
2023-02-04 15:10:00 -0300

    feat: create test function

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,183 +6,103 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 20:38:19 by pmitsuko          #+#    #+#             */
-/*   Updated: 2023/02/03 20:57:28 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2023/02/04 15:08:51 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
-void	bureaucrat_test(void)
+void	shrubbery_creation_form(void)
 {
+	std::cout << "========= SHRUBBERY EXEC FORM ==========" << std::endl;
+	try
 	{
-		std::cout << "========== CONSTRUCTOR ===========" << std::endl;
-		Bureaucrat bob;
-		std::cout << bob << std::endl;
-		Bureaucrat john("John");
-		std::cout << john << std::endl;
-		Bureaucrat ted("Ted", 20);
-		std::cout << ted << std::endl;
-		Bureaucrat ted_copy(ted);
-		std::cout << ted_copy << std::endl;
-		bob = ted_copy;
-		std::cout << bob << std::endl;
+		ShrubberyCreationForm	scf("Shrubbery");
+		Bureaucrat				bob("Bob", 140);
+		scf.beSigned(bob);
+		std::cout << "------" << std::endl;
+		bob.executeForm(scf);
+		std::cout << "------" << std::endl;
+		Bureaucrat				ted("Ted", 135);
+		std::cout << "------" << std::endl;
+		ted.executeForm(scf);
+		std::cout << "------" << std::endl;
+		Bureaucrat				judy("Judy", 148);
+		std::cout << "------" << std::endl;
+		judy.executeForm(scf);
+		std::cout << "------" << std::endl;
 	}
+	catch(const std::exception& e)
 	{
-		std::cout << "============= GRADE ==============" << std::endl;
-		int arr[] = {-20, -1, 0, 1, 3, 5, 80, 142, 149, 150, 151, 162};
-		int arr_len = sizeof(arr) / sizeof (arr[0]);
-		for (int i = 0; i < arr_len; ++i)
-		{
-			std::cout << "Test number = " << arr[i] << std::endl;
-			try
-			{
-				Bureaucrat jimmy("Jimmy", arr[i]);
-				std::cout << jimmy << std::endl;
-			}
-			catch (std::exception& exc)
-			{
-				std::cerr << exc.what() << std::endl;
-			}
-		}
+		std::cerr << e.what() << std::endl;
 	}
-	{
-		std::cout << "=========== INCREMENT ============" << std::endl;
-		try
-		{
-			Bureaucrat judy("Judy", 2);
-			std::cout << judy << std::endl;
-			judy.decrementGrade();
-			std::cout << judy << std::endl;
-			judy.incrementGrade();
-			std::cout << judy << std::endl;
-			judy.incrementGrade();
-			std::cout << judy << std::endl;
-			judy.incrementGrade();
-			std::cout << judy << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-	}
-	{
-		std::cout << "=========== DECREMENT ============" << std::endl;
-		try
-		{
-			Bureaucrat judy("Judy", 149);
-			std::cout << judy << std::endl;
-			judy.incrementGrade();
-			std::cout << judy << std::endl;
-			judy.decrementGrade();
-			std::cout << judy << std::endl;
-			judy.decrementGrade();
-			std::cout << judy << std::endl;
-			judy.decrementGrade();
-			std::cout << judy << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-	}
-	std::cout << "==================================" << std::endl;
+	std::cout << std::endl;
+	std::cout << "========================================" << std::endl;
 }
 
-void	form_test(void)
+void	robotomy_request_form(void)
 {
+	std::cout << "========= ROBOTOMY EXEC FORM ===========" << std::endl;
+	try
 	{
-		std::cout << "========== CONSTRUCTOR ===========" << std::endl;
-		Form form1;
-		std::cout << form1 << std::endl;
-		Form form2("Form 2", 5, 10);
-		std::cout << form2 << std::endl;
-		Form form3("Form 3", 50, 70);
-		std::cout << form3 << std::endl;
-		Bureaucrat teddy("Teddy", 10);
-		std::cout << teddy << std::endl;
-		Form form3_copy(form3);
-		std::cout << form3_copy << std::endl;
-		form3_copy.beSigned(teddy);
-		std::cout << "--- Copy form 3 ---" << std::endl;
-		std::cout << form3_copy << std::endl;
-		std::cout << "--- Base form 3 ---" << std::endl;
-		std::cout << form3 << std::endl;
-		form1 = form3_copy;
-		std::cout << form1 << std::endl;
+		RobotomyRequestForm	rrf("Robot");
+		Bureaucrat				bob("Bob", 72);
+		rrf.beSigned(bob);
+		std::cout << "------" << std::endl;
+		bob.executeForm(rrf);
+		std::cout << "------" << std::endl;
+		Bureaucrat				ted("Ted", 40);
+		std::cout << "------" << std::endl;
+		ted.executeForm(rrf);
+		std::cout << "------" << std::endl;
+		Bureaucrat				judy("Judy", 73);
+		std::cout << "------" << std::endl;
+		judy.executeForm(rrf);
+		std::cout << "------" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
 	}
 	std::cout << std::endl;
+	std::cout << "=========================================" << std::endl;
+}
+
+void	presidential_pardon_form(void)
+{
+	std::cout << "========== PARDON EXEC FORM =============" << std::endl;
+	try
 	{
-		std::cout << "============= GRADE ==============" << std::endl;
-		int arr[] = {-20, -1, 0, 1, 3, 5, 80, 142, 149, 150, 151, 162};
-		int arr_len = sizeof(arr) / sizeof (arr[0]);
-		for (int i = 0; i < arr_len; ++i)
-		{
-			std::cout << "Test number = " << arr[i] << std::endl;
-			try
-			{
-				Form form1("Form 1", arr[i], 10);
-				std::cout << form1 << std::endl;
-			}
-			catch (std::exception& exc)
-			{
-				std::cerr << exc.what() << std::endl;
-			}
-			std::cout << "-------" << std::endl;
-			try
-			{
-				Form form2("Form 2", 20, arr[i]);
-				std::cout << form2 << std::endl;
-			}
-			catch (std::exception& exc)
-			{
-				std::cerr << exc.what() << std::endl;
-			}
-			std::cout << "-------" << std::endl;
-		}
+		PresidentialPardonForm	ppf("Robot");
+		Bureaucrat				bob("Bob", 20);
+		ppf.beSigned(bob);
+		std::cout << "------" << std::endl;
+		bob.executeForm(ppf);
+		std::cout << "------" << std::endl;
+		Bureaucrat				ted("Ted", 2);
+		std::cout << "------" << std::endl;
+		ted.executeForm(ppf);
+		std::cout << "------" << std::endl;
+		Bureaucrat				judy("Judy", 26);
+		std::cout << "------" << std::endl;
+		judy.executeForm(ppf);
+		std::cout << "------" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
 	}
 	std::cout << std::endl;
-	{
-		std::cout << "========= SIGN FORM ==========" << std::endl;
-		try
-		{
-			Bureaucrat july("July", 10);
-			std::cout << july << std::endl;
-			Form form1("Form 1", 10, 20);
-			std::cout << form1 << std::endl;
-			july.signForm(form1);
-			std::cout << form1 << std::endl;
-			july.signForm(form1);
-			std::cout << "-------" << std::endl;
-
-			Form form2("Form 2", 15, 20);
-			std::cout << form2 << std::endl;
-			july.signForm(form2);
-			std::cout << form2 << std::endl;
-			july.signForm(form2);
-			std::cout << "-------" << std::endl;
-
-			Form form3("Form 3", 5, 20);
-			std::cout << form3 << std::endl;
-			july.signForm(form3);
-			std::cout << form3 << std::endl;
-			july.signForm(form3);
-			std::cout << "-------" << std::endl;
-
-			Form form4("Form 4", 4, 20);
-			std::cout << form4 << std::endl;
-			form4.beSigned(july);
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-	}
-	std::cout << "==================================" << std::endl;
+	std::cout << "=========================================" << std::endl;
 }
 
 int	main(void)
 {
-	// bureaucrat_test();
-	form_test();
+	shrubbery_creation_form();
+	robotomy_request_form();
+	presidential_pardon_form();
 	return (0);
 }
