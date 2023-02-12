f2c4d2f01ba59b91e3c2d7c0737a0a17c44c39b4
Mitsu325 <patricia.mashiba@gmail.com>
2023-02-12 17:54:25 -0300

    feat: done ex02

---
+++ cpp06/ex02/Base.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 16:35:19 by pmitsuko          #+#    #+#             */
+/*   Updated: 2023/02/12 16:39:55 by pmitsuko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef BASE_H
+# define BASE_H
+
+# include <iostream>
+
+class Base
+{
+	public:
+		virtual ~Base(void);
+};
+
+#endif
---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 16:44:41 by pmitsuko          #+#    #+#             */
+/*   Updated: 2023/02/12 17:53:12 by pmitsuko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+# include <cstdlib>
+
+Base	*generate(void)
+{
+	if ((rand() % 3) == 0)
+	{
+		return (new A);
+	}
+	else if ((rand() % 3) == 1)
+	{
+		return (new B);
+	}
+	return (new C);
+}
+
+void	identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+	{
+		std::cout << "A" << std::endl;
+	}
+	else if (dynamic_cast<B*>(p))
+	{
+		std::cout << "B" << std::endl;
+	}
+	else if (dynamic_cast<C*>(p))
+	{
+		std::cout << "C" << std::endl;
+	}
+}
+
+void	identify(Base& p)
+{
+	try
+	{
+		A& a = dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+		(void)a;
+	}
+	catch(const std::exception& e)
+	{}
+	try
+	{
+		B& b = dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+		(void)b;
+	}
+	catch(const std::exception& e)
+	{}
+	try
+	{
+		C& c = dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+		(void)c;
+	}
+	catch(const std::exception& e)
+	{}
+}
+
+int	main(void)
+{
+	Base	*ptr1 = generate();
+	Base	*ptr2 = generate();
+	Base	*ptr3 = generate();
+	Base	*ptr4 = generate();
+
+	std::cout << "==================================" << std::endl;
+
+	std::cout << "*ptr1 = ";
+	identify(ptr1);
+	std::cout << "&ptr1 = ";
+	identify(*ptr1);
+	std::cout << std::endl;
+
+	std::cout << "*ptr2 = ";
+	identify(ptr2);
+	std::cout << "&ptr2 = ";
+	identify(*ptr2);
+	std::cout << std::endl;
+
+	std::cout << "*ptr3 = ";
+	identify(ptr3);
+	std::cout << "&ptr3 = ";
+	identify(*ptr3);
+	std::cout << std::endl;
+
+	std::cout << "*ptr4 = ";
+	identify(ptr4);
+	std::cout << "&ptr4 = ";
+	identify(*ptr4);
+	std::cout << std::endl;
+
+	std::cout << "==================================" << std::endl;
+
+	delete ptr1;
+	delete ptr2;
+	delete ptr3;
+	delete ptr4;
+	return (0);
+}
