8196a02222c468a3847881f2481639e45ee6aada
Mitsu325 <patricia.mashiba@gmail.com>
2023-03-02 20:01:02 -0300

    fix: remove if

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 01:17:26 by pmitsuko          #+#    #+#             */
-/*   Updated: 2023/01/11 03:20:25 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:57:03 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,20 +47,28 @@ void	Harl::error(void)
 	std::cout << ERROR_MSG << std::endl;
 }
 
+void	Harl::no_option(void)
+{
+	std::cout << NOT_MSG << std::endl;
+}
+
+int	Harl::resolveOption(std::string level)
+{
+	std::string	option[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int			i = 0;
+
+	while (i < 4 && level != option[i])
+		i++;
+	return (i);
+}
+
 void	Harl::complain(std::string level)
 {
-	int	i = 0;
-	std::string option[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	void (Harl::*func_ptr[4])() = {&Harl::debug, &Harl::info, &Harl::warning,
-		&Harl::error};
+	int		option;
+	void	(Harl::*func_ptr[5])() = {&Harl::debug, &Harl::info, &Harl::warning,
+		&Harl::error, &Harl::no_option};
 
-	while (i < 4)
-	{
-		if (level == option[i])
-		{
-			(this->*(func_ptr[i]))();
-			break;
-		}
-		i++;
-	}
+	option = resolveOption(level);
+	(this->*(func_ptr[option]))();
+	return ;
 }
--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 01:17:23 by pmitsuko          #+#    #+#             */
-/*   Updated: 2023/01/11 02:49:15 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:57:22 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # define INFO_MSG "I cannot believe adding extra bacon costs more money. \nYou didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n"
 # define WARNING_MSG "I think I deserve to have some extra bacon for free. \nI’ve been coming for years whereas you started working here since last month.\n"
 # define ERROR_MSG "This is unacceptable! I want to speak to the manager now.\n"
+# define NOT_MSG "[ Probably complaining about insignificant problems ]"
 
 class Harl
 {
@@ -28,10 +29,13 @@ class Harl
 	void	info(void);
 	void	warning(void);
 	void	error(void);
+	void	no_option(void);
 
 	public:
 		Harl(void);
 		~Harl(void);
+
+		int		resolveOption(std::string level);
 		void	complain(std::string level);
 };
 
