973cabbeb1bb033d30627509b8b81b67b0caec7b
Mitsu325 <patricia.mashiba@gmail.com>
2023-03-02 22:03:38 -0300

    fix: cpp01 ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 17:05:40 by pmitsuko          #+#    #+#             */
-/*   Updated: 2023/01/08 17:13:54 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2023/03/02 21:39:01 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(void)
 	std::string	&stringREF = msg;
 
 	std::cout << "The memory address of the string var: " << &msg << std::endl;
-	std::cout << "The memory address held by stringPTR: " << &stringPTR << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
 	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
 	std::cout << "The value of the string variable:  " << msg << std::endl;
 	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
