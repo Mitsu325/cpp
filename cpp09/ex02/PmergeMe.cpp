f167d79351b07b0948077a2783058f6dd93c130d
Mitsu325 <patricia.mashiba@gmail.com>
2023-04-07 11:05:52 -0300

    refactor: template functions

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 19:14:49 by pmitsuko          #+#    #+#             */
-/*   Updated: 2023/03/26 17:36:10 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2023/04/07 10:57:50 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,6 +84,92 @@ void	PmergeMe::initContainers(char** argv)
 	return ;
 }
 
+template<typename T>
+void	PmergeMe::print(T &num)
+{
+	typename T::iterator	iter;
+
+	for (iter = num.begin(); iter != num.end(); ++iter) {
+		std::cout << *iter << " ";
+	}
+	std::cout << std::endl;
+}
+
+template<typename T>
+void	PmergeMe::mergeInsertSort(T &num, int start_pos, int end_pos)
+{
+	int	middle_pos;
+
+	if (start_pos - end_pos > LIMIT)
+	{
+		middle_pos = (start_pos + end_pos) / 2;
+		this->mergeInsertSort(num, start_pos, middle_pos);
+		this->mergeInsertSort(num, middle_pos + 1, end_pos);
+		this->mergeSort(num, start_pos, middle_pos, end_pos);
+	}
+	else
+		this->insertSort(num, start_pos, end_pos);
+}
+
+template<typename T>
+void	PmergeMe::insertSort(T &num, int start_pos, int end_pos)
+{
+	int	i, temp;
+
+	for (int j = start_pos + 1; j < end_pos; j++)
+	{
+		temp = num[j];
+		i = j - 1;
+		while (i >= start_pos && num[i] > temp)
+		{
+			num[i + 1] = num[i];
+			i--;
+		}
+		num[i + 1] = temp;
+	}
+}
+
+template<typename T>
+void	PmergeMe::mergeSort(T &num, int start_pos, int middle_pos, int end_pos)
+{
+	T	temp;
+	int	temp_idx = start_pos;
+	int	left_idx = start_pos;
+	int	right_idx = middle_pos + 1;
+
+	while ((left_idx <= middle_pos) && (right_idx <= end_pos))
+	{
+		if (num[left_idx] < num[right_idx])
+		{
+			temp[temp_idx] = num[left_idx];
+			left_idx++;
+		}
+		else
+		{
+			temp[temp_idx] = num[right_idx];
+			right_idx++;
+		}
+		temp_idx++;
+	}
+	for (; left_idx <= middle_pos; left_idx++, temp_idx++)
+	{
+		temp[temp_idx] = num[left_idx];
+	}
+	for (; right_idx <= end_pos; right_idx++, temp_idx++)
+	{
+		temp[temp_idx] = num[right_idx];
+	}
+	for (int i = start_pos; i <= end_pos; i++)
+	{
+		num[i] = temp[i];
+	}
+}
+
+double	PmergeMe::elapsedTime(std::clock_t start, std::clock_t end)
+{
+	return (static_cast<double>(end - start) / (CLOCKS_PER_SEC / 1000));
+}
+
 void	PmergeMe::execute(void)
 {
 	std::clock_t				start, end;
@@ -92,16 +178,16 @@ void	PmergeMe::execute(void)
 	std::cout << std::left << std::setw(10) << "Before: ";
 	this->print(this->_vector);
 	start = std::clock();
-	mergeInsertSort(this->_vector, 0, this->_vector.size());
+	this->mergeInsertSort(this->_vector, 0, this->_vector.size());
 	end = std::clock();
-	vec_elapsed_time = static_cast<double>(end - start) / (CLOCKS_PER_SEC / 1000);
+	vec_elapsed_time = this->elapsedTime(start, end);
 	std::cout << std::left << std::setw(10) << "After: ";
 	this->print(this->_vector);
 
 	start = std::clock();
-	mergeInsertSort(this->_deque, 0, this->_deque.size());
+	this->mergeInsertSort(this->_deque, 0, this->_deque.size());
 	end = std::clock();
-	deque_elapsed_time = static_cast<double>(end - start) / (CLOCKS_PER_SEC / 1000);
+	deque_elapsed_time = this->elapsedTime(start, end);
 
 	std::cout << "Time to process a range of " << this->_vector.size();
 	std::cout << " elements with std::vector : " << vec_elapsed_time << " ms";
